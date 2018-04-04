740ed293027309bc4138cec81341e4ec03b886ce
VCM admin account <vcm@vcm-3581.vm.duke.edu>
2018-04-03 23:48:16 -0400

    Updates to heart rate DB to work on remote server

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
 from pymodm import connect
+from flask_cors import CORS 
 import models
 import datetime
 from validate_email import validate_email
 import numpy as np
 
-connect("mongodb://localhost:27017/heart_rate_app")
+connect("mongodb://vcm-3581.vm.duke.edu/heart_rate_app")
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/api/heart_rate", methods=["POST"])
 def heart_rate():
